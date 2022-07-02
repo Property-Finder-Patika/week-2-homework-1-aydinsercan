80826f27b95efa653181513d78fae7aa8d370377
aydinsercan <aydinser17@itu.edu.tr>
2022-07-02 18:08:19 +0300

    Exercises from AkınKaldıroglu

---
+++ Coding_Exercises/Scope.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var bbb = "test2"
+
+func testGlobal() {
+	fmt.Println(bbb)
+}
+
+func main() {
+	var aaa = "test"
+	fmt.Println(aaa)
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+	testLocal()
+	testGlobal()
+}
+
+func testLocal() {
+	//fmt.Println(aaa) //Undefined here
+}
